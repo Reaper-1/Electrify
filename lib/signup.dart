ff5c1968e1eed5fc104d44d496a77b571991b771
Abhi-jith <abhijith200c@gmail.com>
2023-04-07 10:18:54 +0530

    added sign up functionality

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,7 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'main.dart';
 class signup extends StatefulWidget {
@@ -10,6 +12,7 @@ class signup extends StatefulWidget {
 }
 
 class _signupState extends State<signup> {
+  final formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _repasswordController = TextEditingController();
@@ -30,114 +33,150 @@ class _signupState extends State<signup> {
             ],
           ),
         ),
-        child: Center(
-          child: ListView(
-           // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image(image: AssetImage('assets/bulb.gif')),
-                  //SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.only(right: 35,left: 35),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Consumer ID',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5)
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.only(right: 35,left: 35),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Phone Number',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5)
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-                  Container(
-                    padding: EdgeInsets.only(right: 35,left: 35),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.white,
-                    ),
-                    child: TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
+        child: Form(
+          child: Center(
+            child: ListView(
+             // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(image: AssetImage('assets/bulb.gif')),
+                    //SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.only(right: 35,left: 35),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Consumer ID',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5)
                       ),
                     ),
                   ),
-                  SizedBox(height: 10),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.only(right: 35,left: 35),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Phone Number',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5)
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                    Container(
+                      padding: EdgeInsets.only(right: 35,left: 35),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white,
+                      ),
+                      child: TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                        autovalidateMode : AutovalidateMode.onUserInteraction,
+                        validator: (email) =>
+                            email != null && !EmailValidator.validate(email)
+                              ? 'Enter a valid email'
+                              : null,
+                      ),
+                    ),
+                    SizedBox(height: 10),
 
-              SizedBox(height: 10),
-        Container(
-          padding: EdgeInsets.only(right: 35,left: 35),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: Colors.white,
-          ),
-          child: TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(),
+                SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.only(right: 35,left: 35),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.white,
+            ),
+            child: TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (value) => value != null && value.length < 6
+                ? 'Enter min. 6 characters'
+                : null,
             ),
           ),
-        ),
-        SizedBox(height: 10),
-        Container(padding: EdgeInsets.only(right: 35,left: 35),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: Colors.white,
-          ),
-          child: TextField(
-            controller: _confirmPasswordController,
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Confirm Password',
-              border: OutlineInputBorder(),
+          SizedBox(height: 10),
+          Container(padding: EdgeInsets.only(right: 35,left: 35),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.white,
             ),
-          ),
-        ),
-        SizedBox(height: 10),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SizedBox(
-            height: 52,
-            width: 200,
-            child: ElevatedButton(
-              onPressed: () {
-                signup();
-                Navigator.pushNamed(context, '/login');
-                //String email = _emailController.text;
-                String password = _passwordController.text;
-                String confirmPassword = _confirmPasswordController.text;
-              },
-              child: Text('SIGN IN',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),),
-
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
+            child: TextField(
+              controller: _confirmPasswordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Confirm Password',
+                border: OutlineInputBorder(),
               ),
             ),
           ),
-          ),],
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              height: 52,
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  signUp();
+                  Navigator.pushNamed(context, '/login');
+                  //String email = _emailController.text;
+                  String password = _passwordController.text;
+                  String confirmPassword = _confirmPasswordController.text;
+                },
+                child: Text('SIGN IN',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),),
+
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+              ),
+            ),
+            ),],
+                        ),
                       ),
-                    ),
+        ),
                   ),
               );
     }
+
+    Future signUp() async {
+     final isValid = formKey.currentState!.validate();
+     if (!isValid) return;
+
+     showDialog(
+         context: context,
+         barrierDismissible: false,
+         builder: (context) => Center(child: CircularProgressIndicator()),
+     );
+     
+     try{
+       await FirebaseAuth.instance.createUserWithEmailAndPassword(
+           email: _emailController.text.trim(),
+           password: _passwordController.text.trim(),
+       );
+     } on FirebaseAuthException catch (e) {
+       print(e);
+     }
+     navigatorKey.currentState!.popUntil((route) => route.isFirst);
+    }
+  }
+
+  class DatabaseService {
+
   }
 
